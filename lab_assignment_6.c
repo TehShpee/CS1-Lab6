952a59ea0985c829edd0269af27ae488082dc405
TehShpee <teakboy2003@gmail.com>
2024-02-23 15:39:47 -0500

    Completed search function

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int search(int numbers[], int low, int high, int value) 
 {
+	if (low<high){
+	int mid = (high+low)/2;
+	if (value == numbers[mid])
+		return mid;
+	if (value<numbers[mid])
+		return search(numbers, low, mid-1, value);
+	return search(numbers, mid+1, high, value);
+	}
 	return -1;
 }
 
